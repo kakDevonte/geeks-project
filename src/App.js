afa2f8c04767a570547fd061246a2339aa1ecf3e
Aleksandr <kakdevonte@yandex.ru>
2022-10-21 17:34:03 +0700

    add result page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import logo from "./assets/image/PT logo.png";
 import "./App.scss";
 import { PlugPage } from "./page/PlugPage";
 import QuestionsPage from "./page/QuestionsPage";
+import ResultPage from "./page/ResultPage";
 
 const App = () => {
   return (
     <div className="root">
       <img className="logo" src={logo} alt={""} />
       {/*<PlugPage />*/}
-      <QuestionsPage />
+      {/*<QuestionsPage />*/}
+      <ResultPage />
     </div>
   );
 };
---
+++ src/page/ResultPage/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./ResultPage.module.scss";
+import late from "../../assets/image/late-face.png";
+import lose from "../../assets/image/lose-face.png";
+import win from "../../assets/image/win-face.png";
+import logo from "../../assets/image/main-logo.png";
+
+const state = [
+  {
+    image: late,
+    title: [
+      "КТО-ТО ОТВЕТИЛ БЫЧТРЕЕ",
+      <br />,
+      "ЧЕМ ТЫ. НО ИНТУИЦИЯ",
+      <br />,
+      "ТЕБЯ НЕ ПОДВЕЛА",
+    ],
+    subTitle: [
+      "Следующий вопрос",
+      <br />,
+      "появится тут через",
+      <br />,
+      "несколько минут",
+    ],
+  },
+  {
+    image: win,
+    title: ["ПОЗДРАВЛЯЕМ,", <br />, "500 РУБЛЕЙ ТВОИ!"],
+    subTitle: ["Свяжись с нами,", <br />, "чтобы забрать приз"],
+  },
+  {
+    image: lose,
+    title: ["ИЗВИНИ. В ЭТОТ РАЗ", <br />, "ИНТУИЦИЯ", <br />, "ТЕБЯ ПОДВЕЛА."],
+    subTitle: [
+      "Ничего страшного,",
+      <br />,
+      "следующий вопрос",
+      <br />,
+      "появится тут через",
+      <br />,
+      "несколько минут",
+    ],
+  },
+];
+const ResultPage = () => {
+  const status = 2;
+  return (
+    <div className={styles.root}>
+      <div>
+        <img className={styles.face} src={state[status].image} alt={""} />
+        <h1 className={styles.title}>{state[status].title}</h1>
+        <h2 className={styles.subTitle}>{state[status].subTitle}</h2>
+      </div>
+      <div className={styles.footer}>
+        <img className={styles.logo} src={logo} alt={""} />
+        <h1 className={styles.time}>
+          ВТОРНИК
+          <br />
+          <span>19:00</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default ResultPage;
